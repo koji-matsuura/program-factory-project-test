f04e8e073775458ec5d865988589f102ee5d851c
MatsuuraKouji <matsuurakouji@Spice-MacBook-Pro.local>
2026-05-25 12:39:52 +0900

    test: clean up test directories for fresh E2E test

--- rails-sample/app/models/issue.rb
+++
@@ -1,9 +0,0 @@
-class Issue < ApplicationRecord
-  belongs_to :user
-  
-  validates :title, presence: true
-  validates :description, presence: true
-  validates :status, presence: true, inclusion: { in: %w(open closed) }
-  
-  enum status: { open: 'open', closed: 'closed' }
-end
